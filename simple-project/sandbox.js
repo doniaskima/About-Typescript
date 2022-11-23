a0f456d2edac1ee28fe556c3baac1a24724989fb
unknown <doniaskima344@gmail.com>
2022-11-23 01:59:45 +0100

    #4 - Objects & Arrays

--- simple-project/sandbox.js
+++ simple-project/sandbox.js
@@ -1,14 +1,30 @@
 var character = "Donia";
 var age = 30;
 var isBlackBelt = false;
-// character = 20;
-character = "Marwa";
-// age="Jiji" //error
-age = 21;
-// isBlackBelt = "yes"; //error
-isBlackBelt = true;
-//just we can change the values not the types !
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//arrays
+var names = ['Donia', 'jiji', 'Marwa'];
+names.push('Islem');
+// names.push(3); //error
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// number.push('Donia')
+// numbers[1]='Donia'
+var mixed = ['Ken', 4, 'Donia', 8, 9];
+mixed.push('Sirine');
+mixed.push(5);
+mixed[0] = 3;
+//Objects 
+var cuteGirl = {
+    name: 'Donia',
+    belt: 'black',
+    age: 20
 };
-console.log(circ(7.5));
+cuteGirl.age = 21;
+cuteGirl.name = "Don";
+cuteGirl.skills = ["writing", "reading"];
+cuteGirl = {
+    name: 'Doniaaa',
+    belt: 'orangee',
+    age: 20,
+    skills: ['idk']
+};
--- simple-project/sandbox.ts
+++ simple-project/sandbox.ts
@@ -3,22 +3,40 @@ let age = 30;
 let isBlackBelt = false;
 
 
-// character = 20;
-character = "Marwa"
-// age="Jiji" //error
- 
-age = 21;
+//arrays
 
-// isBlackBelt = "yes"; //error
-isBlackBelt = true;
+let names = ['Donia', 'jiji', 'Marwa'];
+names.push('Islem');
+// names.push(3); //error
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+// number.push('Donia')
+// numbers[1]='Donia'
+
+let mixed = ['Ken', 4, 'Donia', 8, 9];
+
+mixed.push('Sirine');
+mixed.push(5);
+mixed[0] = 3;
 
 
-//just we can change the values not the types !
+//Objects 
 
+let cuteGirl = {
+    name: 'Donia',
+    belt: 'black',
+    age: 20
+};
 
-const circ = (diameter :number) => {
-    return diameter * Math.PI;
+cuteGirl.age = 21;
+cuteGirl.name = "Don"
+cuteGirl.skills = ["writing", "reading"];
+cuteGirl = {
+    name: 'Doniaaa',
+    belt: 'orangee',
+    age: 20,
+    skills: ['idk']
 }
-
-console.log(circ(7.5));
 
