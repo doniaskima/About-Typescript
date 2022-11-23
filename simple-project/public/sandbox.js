f75711b88aa37386fa87f9aa9d87dc648a45daa9
unknown <doniaskima344@gmail.com>
2022-11-23 03:00:48 +0100

    #8 - Function Basics

--- simple-project/public/sandbox.js
+++ simple-project/public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("test");
+let greet;
+//greet="hello"
+greet = () => {
+    console.log("hello again");
+};
+const add;
+void ;
+{
+    console.log(a + b);
+    console.log(c);
+}
+add(5, 10, 20);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
