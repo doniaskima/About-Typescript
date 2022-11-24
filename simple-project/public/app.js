145fccbdd6c5f52e5fae66e6eae6d18ae12a5c58
unknown <doniaskima344@gmail.com>
2022-11-24 02:30:39 +0100

    #12 - Classes

--- simple-project/public/app.js
+++ simple-project/public/app.js
@@ -3,12 +3,34 @@
 // console.log(anchor.href);
 // const form = document.querySelector('form')!;
 // console.log(form);
+//Classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("Donia", "learning ts", 383);
+const invTwo = new Invoice("sirine", "learning ts", 383);
+console.log(invOne, invTwo);
+let invoices = [];
 const form = document.querySelector(".new-item-form");
 console.log(form.children);
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = "Dorra";
+invTwo.amount = 788;
+console.log(invOne, invTwo);
+console.log(invoices);
 //inputs 
 const type = document.querySelector('#type');
 const toform = document.querySelector('#toform');
 const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, toform.value, details.value);
--- simple-project/src/app.ts
+++ simple-project/src/app.ts
@@ -3,8 +3,38 @@
 // const form = document.querySelector('form')!;
 // console.log(form);
 
+//Classes
+class Invoice{
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("Donia", "learning ts", 383);
+const invTwo = new Invoice("sirine", "learning ts", 383);
+
+console.log(invOne, invTwo);
+let invoices: Invoice[] = [];
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = "Dorra";
+invTwo.amount = 788;
+console.log(invOne, invTwo);
+
+console.log(invoices);
 
 
 
@@ -12,6 +42,7 @@ console.log(form.children);
 const type = document.querySelector('#type') as HTMLSelectElement;
 const toform = document.querySelector('#toform') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
