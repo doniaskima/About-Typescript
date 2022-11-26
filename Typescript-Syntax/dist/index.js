15433e1798b09cd00e9458f0d4c3ccf35c3359b5
unknown <doniaskima344@gmail.com>
2022-11-26 21:49:54 +0100

    Arrays and Tuples

--- Typescript-Syntax/dist/index.js
+++ Typescript-Syntax/dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
+//Basic Types
 let id = 5;
-console.log('Id :', id);
+let company = "Donia skima";
+let isPublished = false;
+let x = 'Hello';
+let age;
+let ids = [2, 3, 2, 1, 8, 9];
+let arr = [1, true, "hello"];
+//Tuple
+let person = [20, "Donia", true];
+//Tuple Array
+let employee;
+employee = [
+    [1, "Donia"],
+    [2, "Marwa"],
+    [3, "Jiji"],
+];
--- Typescript-Syntax/src/index.ts
+++ Typescript-Syntax/src/index.ts
@@ -1,3 +1,23 @@
+
+//Basic Types
 let id: number = 5;
+let company: string = "Donia skima";
+let isPublished: boolean = false;
+let x: any = 'Hello';
+let age: number;
+
+let ids: number[] = [2, 3, 2, 1, 8, 9];
+let arr: any[] = [1, true, "hello"];
+
+
+//Tuple
+let person: [number, string, boolean] = [20, "Donia", true];
+//Tuple Array
+let employee: [number, string][]
+employee = [
+    [1, "Donia"],
+    [2, "Marwa"],
+    [3,"Jiji"],
+]
+
  
-console.log('Id :', id);
