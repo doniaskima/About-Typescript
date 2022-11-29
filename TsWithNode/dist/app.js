28e8acfd1b9d8878b9c41389ede450a3373dd342
unknown <doniaskima344@gmail.com>
2022-11-29 01:18:59 +0100

    express setup

--- TsWithNode/dist/app.js
+++ TsWithNode/dist/app.js
@@ -1,3 +1,15 @@
 "use strict";
-const greeting = "Hello";
-const numbers = [1, 2, 3];
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const add = (a, b) => a + b;
+app.get("/", (req, res) => {
+    console.log(add(5, 5));
+    res.send("Hello");
+});
+app.listen(5000, () => {
+    console.log("Server running");
+});
--- TsWithNode/src/app.ts
+++ TsWithNode/src/app.ts
@@ -1,4 +1,15 @@
-const greeting:string ="Hello";
+import express, {Application,Request,Response ,NextFunction} from "express";
+
+const app:Application=express();
+
+const add = (a: number, b: number): number => a + b;
+
+app.get("/",(req,res)=>{
+    console.log(add(5, 5));
+    res.send("Hello");
+});
 
 
-const numbers: number[] = [1, 2, 3];
+app.listen(5000, () => {
+    console.log("Server running");
+})
